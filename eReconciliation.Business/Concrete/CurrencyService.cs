95326d7ca6ae4990c9832cb98f14a2d5cea2b3bf
Ahmetk9122 <ahmet.karabudakk.9122@gmail.com>
2024-03-07 23:27:54 +0300

    Currency Service namespace revs

--- eReconciliation.Business/Concrete/CurrencyService.cs
+++ eReconciliation.Business/Concrete/CurrencyService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using eReconciliation.DataAccess;
 
-namespace eReconciliation.Business.Concrete
+namespace eReconciliation.Business
 {
     public class CurrencyService : ICurrencyService
     {
