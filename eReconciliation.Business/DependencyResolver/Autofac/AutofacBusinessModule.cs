3a8399278179a7e8bbce7509f90e78d0132d906a
Ahmetk9122 <ahmet.karabudakk.9122@gmail.com>
2024-03-07 23:27:35 +0300

    --DependencyInjection Updated.

--- eReconciliation.Business/DependencyResolver/Autofac/AutofacBusinessModule.cs
+++ eReconciliation.Business/DependencyResolver/Autofac/AutofacBusinessModule.cs
@@ -13,6 +13,27 @@ namespace eReconciliation.Business.DependencyResolver.Autofac
         {
             builder.RegisterType<CompanyService>().As<ICompanyService>();
             builder.RegisterType<EfCompanyDal>().As<ICompanyDal>();
+
+            builder.RegisterType<AccountReconciliationDetailService>().As<IAccountReconciliationDetailService>();
+            builder.RegisterType<EfAccountReconciliationDetailDal>().As<IAccountReconciliationDetailDal>();
+
+            builder.RegisterType<AccountReconciliationService>().As<IAccountReconciliationService>();
+            builder.RegisterType<EfAccountReconciliationDal>().As<IAccountReconciliationDal>();
+
+            builder.RegisterType<BaBsReconciliationDetailService>().As<IBaBsReconciliationDetailService>();
+            builder.RegisterType<EfBaBsReconciliationDetailDal>().As<IBaBsReconciliationDetailDal>();
+
+            builder.RegisterType<BaBsReconciliationService>().As<IBaBsReconciliationService>();
+            builder.RegisterType<EfBaBsReconciliationDal>().As<IBaBsReconciliationDal>();
+
+            builder.RegisterType<CurrencyAccountService>().As<ICurrencyAccountService>();
+            builder.RegisterType<EfCurrencyAccountDal>().As<ICurrencyAccountDal>();
+
+            builder.RegisterType<CurrencyService>().As<ICurrencyService>();
+            builder.RegisterType<EfCurrencyDal>().As<ICurrencyDal>();
+
+            builder.RegisterType<MailParameterService>().As<IMailParameterService>();
+            builder.RegisterType<EfMailParameterDal>().As<IMailParameterDal>();
         }
     }
 }
