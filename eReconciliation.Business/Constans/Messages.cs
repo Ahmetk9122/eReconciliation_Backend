2f7cd36119d114ce97aa03958b7999f0bc430c42
Ahmetk9122 <ahmet.karabudakk.9122@gmail.com>
2024-03-15 00:24:21 +0300

    Result messages updated.

--- eReconciliation.Business/Constans/Messages.cs
+++ eReconciliation.Business/Constans/Messages.cs
@@ -9,5 +9,8 @@ namespace eReconciliation.Business.Constans
     {
         public static string AddedCompany = "Şirket kaydı başarıyla kaydedildi.";
         public static string List = "Listeleme işlemi başarılı";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola Hatalı";
+        public static string SuccessfulLogin = "Giriş Başarılı";
     }
 }
