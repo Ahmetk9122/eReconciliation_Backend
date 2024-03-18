684b808c6758963fb1dbc2b9074eb7faf60a4ee9
Ahmetk9122 <ahmet.karabudakk.9122@gmail.com>
2024-03-18 09:23:39 +0300

    login register revs

--- eReconciliation.Business/Abstract/IAuthService.cs
+++ eReconciliation.Business/Abstract/IAuthService.cs
@@ -1,16 +1,16 @@
 using eReconciliation.Core.Entities.Concrete;
 using eReconciliation.Core.Utilities;
 using eReconciliation.Core.Utilities.Results.Abstract;
-using eReconciliation.Entities.Dtos;
+using eReconciliation.Entities;
 
 namespace eReconciliation.Business
 {
     public interface IAuthService
     {
-        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
-        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        IDataResult<User> Register(UserForRegister userForRegister, string password);
+        IDataResult<User> Login(UserForLogin userForLogin);
         IResult UserExist(string email);
-        IDataResult<AccessToken> CreateAccessToken(User user);
+        IDataResult<AccessToken> CreateAccessToken(User user, int companyId);
 
     }
 }
--- eReconciliation.Business/Concrete/AuthService.cs
+++ eReconciliation.Business/Concrete/AuthService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using eReconciliation.Business.Constans;
 using eReconciliation.Core;
@@ -8,6 +9,7 @@ using eReconciliation.Core.Entities.Concrete;
 using eReconciliation.Core.Utilities;
 using eReconciliation.Core.Utilities.Results.Abstract;
 using eReconciliation.Core.Utilities.Results.Concrete;
+using eReconciliation.Entities;
 using eReconciliation.Entities.Dtos;
 
 namespace eReconciliation.Business.Concrete
@@ -15,24 +17,30 @@ namespace eReconciliation.Business.Concrete
     public class AuthService : IAuthService
     {
         private readonly IUserService _userService;
-        public AuthService(IUserService userService)
+        private readonly ITokenHelper _tokenHelper;
+
+        public AuthService(IUserService userService, ITokenHelper tokenHelper)
         {
+            _tokenHelper = tokenHelper;
             _userService = userService;
 
         }
-        public IDataResult<AccessToken> CreateAccessToken(User user)
+        public IDataResult<AccessToken> CreateAccessToken(User user, int companyId)
         {
-            throw new NotImplementedException();
+            var claims = _userService.GetClaims(user, companyId);
+            var accessToken = _tokenHelper.CreateToken(user, claims, companyId);
+
+            return new SuccessDataResult<AccessToken>(accessToken);
         }
 
-        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        public IDataResult<User> Login(UserForLogin userForLogin)
         {
-            var userToCheck = _userService.GetByMail(userForLoginDto.Email);
+            var userToCheck = _userService.GetByMail(userForLogin.Email);
             if (userToCheck == null)
             {
                 return new ErrorDataResult<User>(Messages.UserNotFound);
             }
-            if (!HashingHelper.VerifyPasswordHash(userForLoginDto.Password, userToCheck.PasswordHash, userToCheck.PasswordSalt))
+            if (!HashingHelper.VerifyPasswordHash(userForLogin.Password, userToCheck.PasswordHash, userToCheck.PasswordSalt))
             {
                 return new ErrorDataResult<User>(Messages.PasswordError);
             }
@@ -40,14 +48,32 @@ namespace eReconciliation.Business.Concrete
 
         }
 
-        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+        public IDataResult<User> Register(UserForRegister userForRegister, string password)
         {
-            throw new NotImplementedException();
+            byte[] passwordHash, passwordSalt;
+            HashingHelper.CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            var user = new User()
+            {
+                Email = userForRegister.Email,
+                AddedAt = DateTime.Now,
+                IsActive = true,
+                MailConfirm = false,
+                MailConfirmDate = DateTime.Now,
+                MailConfirmValue = Guid.NewGuid().ToString(),
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Name = userForRegister.Name
+            };
+            _userService.Add(user);
+            return new SuccessDataResult<User>(user, Messages.UserRegistered);
         }
 
         public IResult UserExist(string email)
         {
-            throw new NotImplementedException();
+            if (_userService.GetByMail(email) != null)
+                return new ErrorResult(Messages.UserExist);
+            return new SuccessResult();
         }
+
     }
 }
