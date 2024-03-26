de46f55aecddbe9c7cd69e5e99fd0dd679370d24
Ahmetk9122 <ahmet.karabudakk.9122@gmail.com>
2024-03-26 09:24:22 +0300

    --Login and Register updated.

--- eReconciliation.Business/Concrete/AuthService.cs
+++ eReconciliation.Business/Concrete/AuthService.cs
@@ -10,6 +10,7 @@ using eReconciliation.Core.Utilities;
 using eReconciliation.Core.Utilities.Results.Abstract;
 using eReconciliation.Core.Utilities.Results.Concrete;
 using eReconciliation.Entities;
+using eReconciliation.Entities.Concrete;
 using eReconciliation.Entities.Dtos;
 
 namespace eReconciliation.Business.Concrete
@@ -18,13 +19,21 @@ namespace eReconciliation.Business.Concrete
     {
         private readonly IUserService _userService;
         private readonly ITokenHelper _tokenHelper;
+        private readonly ICompanyService _companyService;
 
-        public AuthService(IUserService userService, ITokenHelper tokenHelper)
+        public AuthService(IUserService userService, ITokenHelper tokenHelper, ICompanyService companyService)
         {
             _tokenHelper = tokenHelper;
+            _companyService = companyService;
             _userService = userService;
 
         }
+
+        public IResult CompanyExist(Company company)
+        {
+            return _companyService.CompanyExists(company);
+        }
+
         public IDataResult<AccessToken> CreateAccessToken(User user, int companyId)
         {
             var claims = _userService.GetClaims(user, companyId);
@@ -48,7 +57,57 @@ namespace eReconciliation.Business.Concrete
 
         }
 
-        public IDataResult<User> Register(UserForRegister userForRegister, string password)
+        public IDataResult<UserCompanyDto> Register(UserForRegister userForRegister, string password, Company company)
+        {
+            #region [ Kullanıcı Kayıdı ]
+            byte[] passwordHash, passwordSalt;
+            HashingHelper.CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            var user = new User()
+            {
+                Email = userForRegister.Email,
+                AddedAt = DateTime.Now,
+                IsActive = true,
+                MailConfirm = false,
+                MailConfirmDate = DateTime.Now,
+                MailConfirmValue = Guid.NewGuid().ToString(),
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Name = userForRegister.Name
+            };
+            _userService.Add(user);
+            #endregion
+
+            #region [ Şirket Kayıdı ]
+
+            _companyService.Add(company);
+
+            #endregion
+
+            #region [ Kullanıcı Şirket Mapping Kayıdı ]
+
+            _companyService.UserCompanyMapingAdd(user.Id, company.Id);
+
+            #endregion
+
+            UserCompanyDto userCompanyDto = new UserCompanyDto()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                AddedAt = user.AddedAt,
+                CompanyId = company.Id,
+                IsActive = true,
+                MailConfirm = user.MailConfirm,
+                MailConfirmDate = user.MailConfirmDate,
+                MailConfirmValue = user.MailConfirmValue,
+                PasswordHash = user.PasswordHash,
+                PasswordSalt = user.PasswordSalt
+            };
+
+            return new SuccessDataResult<UserCompanyDto>(userCompanyDto, Messages.UserRegistered);
+        }
+
+        public IDataResult<User> RegisterSecondAccount(UserForRegister userForRegister, string password)
         {
             byte[] passwordHash, passwordSalt;
             HashingHelper.CreatePasswordHash(password, out passwordHash, out passwordSalt);
